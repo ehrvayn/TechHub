7e29054ac1884c7deb73aed03c546b3d6f62ba37
ehrvayn <rjmomong@gmail.com>
2026-09-09 16:34:32 +0800

    Style: adjust lagout button

--- components/admin/SideBar.tsx
+++ components/admin/SideBar.tsx
@@ -169,7 +169,7 @@ export default function Sidebar() {
           </span>
         </button>
         <div
-          className={`space-y-3 border-t px-4 pt-3 ${
+          className={`space-y-3 border-t -mx-4 h-0.5 px-4 py-3 ${
             isDarkMode
               ? "border-zinc-800 bg-zinc-950"
               : "border-zinc-200 bg-white"
